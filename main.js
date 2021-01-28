2edc4e37687674d80ef71ab58fefb105213cb1a7
Mia <micaelasgilbert@gmail.com>
2021-01-28 04:00:23 -0600

    Add additional class properties and the playGame method

--- main.js
+++ main.js
@@ -8,18 +8,66 @@ const pathCharacter = '*';
 class Field {
     constructor(field) {
         this.field = field;
+        this.outerIndex = 0;
+        this.innerIndex = 0;
+        this.field[0][0] = pathCharacter;
+        this.won = false;
+        this.loss = false;
     }
 
     print() {
         this.field.forEach(el => console.log(el.join('')));
     }
 
-    static generateField(height, width, percentage) {
+    playGame() {
+        while (!this.won && !this.loss) {
+            this.print();
+            let direction = prompt('Which way? (d = Down, u = Up, l = left & r = right): ').toLowerCase();
+        
+            if (direction === 'l') {
+                this.innerIndex--;
+            }
+            else if (direction === 'r') {
+                this.innerIndex++;
+            }
+            else if (direction === 'u') {
+                this.outerIndex--;
+            }
+            else if (direction === 'd') {
+                this.outerIndex++;
+            }
+            else {
+                console.log('Please enter d, u, l or r');
+            }
+            if (this.innerIndex < 0 || this.innerIndex > this.field[0].length - 1 || this.outerIndex < 0 || this.outerIndex > this.field.length - 1) {
+                console.log('You went out of bounds!');
+                console.log('Sorry, you lost the game :(');
+                this.loss = true;
+            }
+            else if (this.field[this.outerIndex][this.innerIndex] === hole) {
+                console.log('You fell down a hole!');
+                console.log('Sorry, you lost the game :(');
+                this.loss = true;
+            }
+            else if (this.field[this.outerIndex][this.innerIndex] === hat) {
+                console.log('You found your hat!');
+                console.log('Congrats, you won the game :)');
+                this.won = true;
+            }
+            else {
+                this.field[this.outerIndex][this.innerIndex] = pathCharacter;
+            }
+        }
+    }
+
+    static generateField(height = 3, width = 3, percentage = 0.25) {
         let newField = [];
+        let hatOuterIndex;
+        let hatInnerIndex;
         function randomRow() {
             let row = [];
             const fieldComponents = [hole, fieldCharacter];
-            while(row.length < width) {
+            while (row.length < width) {
                 let randomComponent = fieldComponents[Math.floor(Math.random() * fieldComponents.length)];
                 row.push(randomComponent);
             }
@@ -36,32 +84,23 @@ class Field {
             }
             return newRow;
         }
-        while(newField.length < height) {
+        while (newField.length < height) {
             let rowElement = addRow();
             newField.push(rowElement);
         }
         function addHat() {
-            let randFieldIndex = Math.floor(Math.random() * newField.length);
-            let randRowIndex = Math.floor(Math.random() * width);
-            newField[randFieldIndex][randRowIndex] = hat;
-        }
-        function addStartPos() {
-            newField[0][0] = pathCharacter;
+            hatOuterIndex = Math.floor(Math.random() * height);
+            hatInnerIndex = Math.floor(Math.random() * width);
+            while (hatOuterIndex === 0 && hatInnerIndex === 0) {
+                hatOuterIndex = Math.floor(Math.random() * height);
+                hatInnerIndex = Math.floor(Math.random() * width);
+            }
+            newField[hatOuterIndex][hatInnerIndex] = hat;
         }
         addHat();
-        addStartPos();
-        console.log(newField);
         return newField;
     }
 }
 
-const practiceGame = new Field(
-    [
-    [ '*', '░', '░', '░' ],
-    [ '░', '░', 'O', '░' ],
-    [ '░', '^', '░', 'O' ],
-    [ '░', '░', 'O', '░' ]
-]);
-
-practiceGame.print();
-//Field.generateField(4, 4, .25);
+const newGame = new Field(Field.generateField(6, 6, 0.3));
+newGame.playGame();
